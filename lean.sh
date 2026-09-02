55a3eb7c28f64a64911041e09e10fe259ea660d9
hza81007155 <800755@qq.com>
2026-09-02 17:48:00 +0800

    Update lean.sh

--- lean.sh
+++ lean.sh
@@ -318,6 +318,16 @@ add_custom_packages() {
     clone_all https://github.com/linkease/istore-ui
     clone_all https://github.com/linkease/istore luci
 
+    # istore
+    git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
+    git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
+    git clone --depth=1 -b main https://github.com/linkease/istore package/istore
+
+    # 修改版本为编译日期
+    date_version=$(date +"%y.%m.%d")
+    orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+    sed -i "s/${orig_version}/R${date_version} by hza800755/g" package/lean/default-settings/files/zzz-default-settings
+
     # 科学上网插件
     clone_all https://github.com/fw876/helloworld
     clone_all https://github.com/Openwrt-Passwall/openwrt-passwall-packages
